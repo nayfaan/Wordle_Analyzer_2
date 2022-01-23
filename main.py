268aca64f76bf21d99f7dedd1ba2cad3d8e53ff1
nayfaan <luknnn129@gmail.com>
2022-01-23 04:28:46 -0500

    v1

--- main.py
+++ main.py
@@ -1,55 +1,77 @@
-import services.match
-import itertools
-#import math
+from copy import deepcopy as dcopy
 
-def index_display(match, sol):
-    disp = []
-    digit = len(str(len(sol)))
-    for word in match.comb:
-        disp.append(str(sol.index(word)).rjust(digit))
-    return disp
+def strip_0 (__frequency):
+    frequency_temp = []
+    for pos in __frequency:
+        temp = dcopy(pos)
+        
+        for alpha, freq in pos.items():
+            if freq == 0:
+                temp.pop(alpha)        
+        frequency_temp.append(temp)
+    return frequency_temp
 
-def run(length, compare):
-    if not isinstance(length, int):
-        raise TypeError("Word length must be an integer.")
-    if length < 2 or length > 15:
-        raise ValueError("Word length must be between 2-15.")
-    if not isinstance(compare, int):
-        raise TypeError("No. of words to compare must be an integer.")
-    if compare < 2:
-        raise ValueError("No. of words to compare must be at least 2.")
-    
-    with open("./input/split/dictionary_"+str(length)+".txt", "rt") as f:
-        sol = f.read().splitlines()
-                
-    max_coverage = 1
-    
-    length_of_dict = len(sol)
-    print("# of words with "+str(length)+" letters: "+str(length_of_dict))
-    print_interval = 1000000
+def find_words_within_first_freq(frequency_sans_num):
     count = 0
+    words_within_first_freq = []
     
-    match_comb = itertools.combinations(sol, compare)
-    good_set = []
-    #threshold = 0#int(math.ceil((2-pow(2,1-compare))*length))
-    for comb in match_comb:
-        comb_match = services.match.match(comb)
-        if comb_match.cover == max_coverage:
-            good_set.append(comb_match)
-        elif comb_match.cover > max_coverage:
-            good_set = [comb_match]
-            max_coverage = comb_match.cover
+    while count < 26:
+        alpha_freq_slice = [pos[:count+1] for pos in frequency_sans_num]
+        
+        for word in sol_all:
+            word_list = list(word)
+            if word_list[0] in alpha_freq_slice[0] and word_list[1] in alpha_freq_slice[1] and word_list[2] in alpha_freq_slice[2] and word_list[3] in alpha_freq_slice[3] and word_list[4] in alpha_freq_slice[4]:
+                words_within_first_freq.append(word)
+        
+        if len(words_within_first_freq) > 0:
+            break
         
         count += 1
-        if count % print_interval == 0:
-            print("Analyzed: [",", ".join(index_display(comb_match, sol)),"] ...",sep='')
-            
-    #good_set.sort(key=len)
-    for x in good_set:
-        if x.cover == max_coverage:
-            print(x)
+        
+    print(words_within_first_freq)
+    
+def score_word(word):
+    score = 0
+    
+    for index, alpha in enumerate(list(word)):
+        score += frequency[index][alpha]
+    return score
+
+def assign_freq_to_words():
+    word_score_dict = {word: score_word(word) for word in sol_all}
+    word_score_dict = dict(sorted(word_score_dict.items(), key = lambda item: item[1], reverse = True))
+    print(word_score_dict)
+
+def run():
+        
+    alpha = list('abcdefghijklmnopqrstuvwxyz')
+    temp = {}
+    for a in alpha:
+        temp[a] = 0
+    
+    global frequency
+    frequency = [dcopy(temp) for _ in range(5)]
+    
+    for word in sol:
+        for index, a in enumerate(list(word)):
+            frequency[index][a] += 1
+    
+    frequency = [dict(sorted(pos.items(), key = lambda item: item[1], reverse = True)) for pos in frequency]
+    
+    #frequency = strip_0(frequency)
+    
+    global frequency_sans_num
+    frequency_sans_num = [list(pos) for pos in frequency]
+    
+    find_words_within_first_freq(frequency_sans_num)
+    print()
+    assign_freq_to_words()
 
 if __name__ == "__main__":
-    length = 4
-    compare = 3
-    run(length, compare)
+    with open("./input/possible_solutions.txt", "rt") as f:
+        sol = f.read().splitlines()
+    with open("./input/additional_words.txt", "rt") as f:
+        sol_add = f.read().splitlines()
+    sol_all = sol + sol_add
+    
+    run()
